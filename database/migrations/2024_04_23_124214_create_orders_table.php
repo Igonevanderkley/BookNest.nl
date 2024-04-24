47609d9aa5569bf6265c8231feac091ad03e5c7e
Igone van_der_Kley <igone@vdkley.nl>
2024-04-24 10:20:21 +0200

    Migrations altered

--- database/migrations/2024_04_23_124214_create_orders_table.php
+++ database/migrations/2024_04_23_124214_create_orders_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('roleId');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('userId');
+            $table->dateTime('orderDate');
+            $table->integer('totalQuantity');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
